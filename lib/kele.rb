7e75507e3013452797dd3b7db49e95c4adaf7d29
Caroline Vurlumis <cvurlumis@gmail.com>
2017-12-10 13:05:37 +0100

    user-story-3, retieve own data as a Ruby hash

--- lib/kele.rb
+++ lib/kele.rb
@@ -1,4 +1,5 @@
 require 'httparty'
+require 'json'
 
 class Kele
 
@@ -6,9 +7,15 @@ class Kele
   base_uri 'https://www.bloc.io/api/v1'
 
   def initialize(email, password)
-    @user_auth_code = self.class.post("/sessions", body: { "email": email, "password": password })
-    puts @user_auth_code.code
-    raise 'Invalid email or password' if @user_auth_code.code != 200
+    user_info = self.class.post("/sessions", body: { "email": email, "password": password })
+    @user_auth_code = user_info['auth_token']
+    puts @user_auth_code
+    raise 'Invalid email or password' if user_info.code != 200
+  end
+
+  def get_me #return current user from Bloc API
+    response = self.class.get("/users/me", headers: { "authorization" => @user_auth_code })
+    user_data = JSON.parse(response.body)
   end
 
 end
