a04b57d414d1982a2138b2d13971d847646cd736
Caroline Vurlumis <cvurlumis@gmail.com>
2017-12-10 21:05:14 +0100

    user-story-4, added mentor availability method

--- lib/kele.rb
+++ lib/kele.rb
@@ -15,7 +15,13 @@ class Kele
 
   def get_me #return current user from Bloc API
     response = self.class.get("/users/me", headers: { "authorization" => @user_auth_code })
-    user_data = JSON.parse(response.body)
+    JSON.parse(response.body)
+  end
+
+  def get_mentor_availability(mentor_id) #Ben's mentor id == 537104
+    response = self.class.get("/mentors/#{mentor_id}/student_availability", headers: { "authorization" => @user_auth_code })
+    JSON.parse(response.body)
+
   end
 
 end
